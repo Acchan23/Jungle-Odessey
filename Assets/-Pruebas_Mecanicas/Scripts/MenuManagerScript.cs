32849cdcb441ee9f8b0adf275ae57ba78a70c7ad
hrojast93 <hrojast93@gmail.com>
2024-08-15 23:56:27 -0500

    StartMenu
    Se agrega la escena de Start para el ciclo básico de pruebas de mecánicas.

---
+++ Assets/-Pruebas_Mecanicas/Scripts/MenuManagerScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManagerScript : MonoBehaviour
+{
+    //Carga las escenas del jeugo
+    public void EscenaJuego(){
+        SceneManager.LoadScene("SebasGym");
+    }
+}
